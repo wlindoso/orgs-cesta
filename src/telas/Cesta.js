6899b8e6b33fce3b00cd91b668875b1c79802326
Waldeck Lindoso Jr <wlindoso@gmail.com>
2023-08-07 12:28:53 -0300

    first commit

---
+++ src/telas/Cesta.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
+
+import topo from "../../assets/topo.png";
+import logo from "../../assets/logo.png";
+
+const width = Dimensions.get("screen").width;
+
+export default function cesta() {
+  return (
+    <>
+      <Image source={topo} style={estilos.topo} />
+      <Text style={estilos.titulo}>Detalhes da cesta</Text>
+      <View style={estilos.cesta}>
+        <Text style={estilos.nome}>Cesta de Verduras</Text>
+        <View style={estilos.fazenda}>
+          <Image source={logo} style={estilos.imagemFazenda} />
+          <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        </View>
+        <Text style={estilos.descricao}>
+          Uma cesta com produtos selecionados cuidadosamente da fazenda direto
+          para sua cozinha
+        </Text>
+        <Text style={estilos.preco}>R$ 40,00</Text>
+      </View>
+    </>
+  );
+}
+
+const estilos = StyleSheet.create({
+  topo: {
+    width: "100%",
+    height: (578 / 768) * width,
+  },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 26,
+    color: "white",
+    // fontWeight: "bold",
+    fontFamily: "MontserratBold",
+    padding: 16,
+  },
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    // fontWeight: "bold",
+    fontFamily: "MontserratBold",
+  },
+  fazenda: {
+    display: "flex",
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+  imagemFazenda: {
+    width: 32,
+    height: 32,
+  },
+  nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontFamily: "MontserratRegular",
+  },
+  descricao: {
+    color: "#a3a3a3",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  preco: {
+    color: "#2a9f85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
+  },
+});
